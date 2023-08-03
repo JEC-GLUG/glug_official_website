b00839fe20a6beb4d10657eb04029326ab1bd115
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-08-03 14:25:57 +0530

    Add constants in index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,18 +1,80 @@
-export const navLinks = [
+
+// ******  Navigation Links  ****** 
+const navLinks = [
     {
-      id: "resources",
-      title: "Resources",
+        id: "resources",
+        title: "Resources",
     },
     {
-      id: "projects",
-      title: "Projects",
+        id: "projects",
+        title: "Projects",
     },
     {
-      id: "alumni",
-      title: "Alumni",
+        id: "alumni",
+        title: "Alumni",
     },
     {
-      id: "join",
-      title: "Join Us",
+        id: "join",
+        title: "Join Us",
     },
 ];
+
+
+// ******  List of Projects  ****** 
+const projects = [
+    {
+        name: "",
+        image: "",
+        details: "",
+    },
+    {
+        name: "",
+        image: "",
+        details: "",
+    },
+];
+
+
+// ******  List of Current Members  ****** 
+const currentMembers = [
+    {
+        name: "",
+        position: "",
+        batch: "",
+        linkedIn: "",
+        github: "",
+        instagram: "",
+    },
+    {
+        name: "",
+        position: "",
+        batch: "",
+        linkedIn: "",
+        github: "",
+        instagram: "",
+    },
+];
+
+
+// ******  List of Past Members ****** 
+const pastMembers = [
+    {
+        name: "",
+        position: "",
+        batch: "",
+        linkedIn: "",
+        github: "",
+        instagram: "",
+      },
+      {
+        name: "",
+        position: "",
+        batch: "",
+        linkedIn: "",
+        github: "",
+        instagram: "",
+      },
+];
+
+export { navLinks, projects, currentMembers, pastMembers };
+
