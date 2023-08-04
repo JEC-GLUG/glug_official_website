47046e5f00dbbde7adf919c2a9d1bc90578192c9
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-08-05 02:16:48 +0530

    Fix scroll animation on Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,17 +49,23 @@ const Hero = () => {
       </motion.div>
 
       {/* <!-----------------  Scroll Animation -----------------> */}
-      <div
-        className="absolute xs:bottom-10 bottom-32 w-full md:flex justify-center items-center hidden"
-      >
-        <a href="#">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-glugBlue border-opacity-75 flex justify-center items-start p-2">
-            <div
-              className="w-3 h-3 rounded-full bg-glugBlue bg-opacity-75 mb-1"
-            />
-          </div>
-        </a>
-      </div>
+      <div className="absolute xs:bottom-10 bottom-32 w-full md:flex justify-center items-center hidden">
+                <a href="#">
+                    <div className="w-[35px] h-[64px] rounded-3xl border-4 border-glugBlue border-opacity-75 flex justify-center items-start p-2">
+                        <motion.div
+                            animate={{
+                                y: [0, 24, 0],
+                            }}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType: "loop",
+                            }}
+                            className="w-3 h-3 rounded-full bg-glugBlue bg-opacity-75 mb-1"
+                        />
+                    </div>
+                </a>
+            </div>
     </section>
   );
 };
