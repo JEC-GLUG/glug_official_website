53ebd21229a88c61a1f7501186d7983f9c9b37fd
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-10-07 02:58:59 +0530

    Form Submission Works

--- src/components/JoinUsForm.jsx
+++ src/components/JoinUsForm.jsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
 import CustomTextField from "./CustomTextField";
-import { Button } from "@mui/material";
+import { Alert, Button, Snackbar } from "@mui/material";
+import { styled } from "@mui/styles";
+
+
+// const StyledSnackbar = styled((props) => <Snackbar {...props} />)(
+//     ({ theme }) => ({
+//         "& .MuiSnackbar-root": {
+//             top: theme.spacing(15),
+//         },
+//     })
+// );
+
 
 const JoinUsForm = () => {
+    // Constants
+    const SUCCESS = "SUCCESS";
+
+
+    // State Variables
     const [userData, setUserData] = useState({
-        name: "",
-        email: "",
-        department: "",
-        year: "",
+        Name: "",
+        Email: "",
+        Department: "",
+        Year: "",
     });
 
     // Handle Change in Input Field
@@ -16,78 +32,117 @@ const JoinUsForm = () => {
         setUserData({ ...userData, [e.target.name]: value.trim() });
     };
 
+
+    // Toogle Snackbar state
+    const [alertType, setAlertType] = useState({
+        open: false,
+        type: "success",
+    });
+
+    // Handle Closing of Alert
+     const handleSnackClose = () => {
+        setAlertType({...alertType, open: false});
+    }
+
+
     // Handle Form Submission
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const formElement = document.querySelector("join-us-form");
-        const data = new FormData(formElement);
-        const apiURL = process.env.REACT_APP_FORM_URL;
+        const apiURL = import.meta.env.VITE_FORM_URL;
 
-        fetch(apiURL, {
+        console.log(apiURL);
+        console.log(userData);
+
+        const response = await fetch(apiURL, {
             method: "POST",
-            body: data,
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-            })
-            .catch((error) => {
-                console.log(error);
+            // mode: "no-cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        });
+        if (response.ok) {
+            // Form submitted successfully
+            setAlertType({type: "success", open: true});
+            setUserData({
+                Name: "",
+                Email: "",
+                Department: "",
+                Year: "",
             });
+        } else {
+            // Handle error
+            // console.error("Error submitting form");
+            // setAlertType({type: "success", open: true});
+            // setUserData({
+            //     Name: "",
+            //     Email: "",
+            //     Department: "",
+            //     Year: "",
+            // });
+        }
     };
 
     return (
         <div className="">
-            <form
-                className="join-us-form flex flex-col align-middle justify-center self-center"
-                onSubmit={(e) => handleSubmit(e)}
-            >
+            <form className="join-us-form flex flex-col align-middle justify-center self-center">
                 <CustomTextField
                     label="Name"
                     type="text"
                     margin="normal"
-                    name="name"
-                    value={userData.name}
+                    name="Name"
+                    value={userData.Name}
                     onChange={(e) => handleChange(e)}
                 />
                 <CustomTextField
                     label="Email"
                     type="email"
                     margin="normal"
-                    name="email"
-                    value={userData.email}
+                    name="Email"
+                    value={userData.Email}
                     onChange={(e) => handleChange(e)}
                 />
                 <CustomTextField
                     label="Department"
                     type="text"
                     margin="normal"
-                    name="department"
-                    value={userData.department}
+                    name="Department"
+                    value={userData.Department}
                     onChange={(e) => handleChange(e)}
                 />
                 <CustomTextField
                     label="Year"
                     type="text"
                     margin="normal"
-                    name="year"
-                    value={userData.year}
+                    name="Year"
+                    value={userData.Year}
                     onChange={(e) => handleChange(e)}
                 />
                 <Button
+                    onClick={handleSubmit}
                     variant="contained"
                     color="success"
                     size="large"
                     sx={{
                         width: "10rem",
                         alignSelf: "center",
-                        marginTop: "0.6rem"
+                        marginTop: "0.6rem",
                     }}
                 >
                     Submit
                 </Button>
             </form>
+            <Snackbar
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                open={alertType.open}
+                onClose={handleSnackClose}
+                autoHideDuration={3000}
+            >
+                <Alert severity={alertType.type} onClose={handleSnackClose}>
+                    Your response has been recorded.
+                </Alert>
+            </Snackbar>
         </div>
     );
 };
