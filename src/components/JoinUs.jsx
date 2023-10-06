7c82f3a521b777e3c1725ac02369b1feebb52647
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-10-06 23:05:26 +0530

    fix

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -3,7 +3,6 @@ import { styles } from "../styles";
 
 import JoinUsForm from "./JoinUsForm";
 
-import { quote } from "../assets";
 
 const JoinUs = () => {
     return (
@@ -11,14 +10,14 @@ const JoinUs = () => {
             <section
                 className={`relative w-full h-screen mx-auto overflow-hidden text-white flex flex-wrap ${styles.paddingX} mt-4 font-inter`}
             >
-                <div className="w-full flex items-center justify-center relative mt-[9rem]">
+                <div className="w-full flex items-center justify-center relative mt-[12rem]">
                     <h1 className={`${styles.headingText}`} id="join-us">
                         Join <span className="text-glugBlue">U</span>s
                     </h1>
                 </div>
 
                 {/* Form */}
-                <div className="w-full flex flex-row align-middle justify-center -mt-[5.8rem]">
+                <div className="w-full flex flex-row align-middle justify-center -mt-[3rem]">
                     <div className=" h-[26rem] w-[32rem] px-6 pt-4">
                         <JoinUsForm />
                     </div>
